47f087f19b3a096a78a96ed1cdddb50e3df20344
TaylorValk <Tvalk06@icloud.com>
2025-11-10 13:38:25 -0500

    Setting Up Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    InputAction moveAction;
+
+    Vector3 moveVector;
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        moveVector = moveAction.ReadValue<Vector2>();
+
+        transform.position += moveVector * moveSpeed * Time.deltaTime;
+    }
+}

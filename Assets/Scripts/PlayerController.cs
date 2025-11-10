b2d3a09c1931aa815070437db83140be548487fa
TaylorValk <Tvalk06@icloud.com>
2025-11-10 13:55:09 -0500

    Viewport Boundaries

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,26 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float leftBoundPadding;
+    [SerializeField] float rightBoundPadding;
+    [SerializeField] float upBoundPadding;
+    [SerializeField] float downBoundPadding;
 
     InputAction moveAction;
 
     Vector3 moveVector;
+    Vector2 minBounds;
+    Vector2 maxBounds;
 
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+
+        InitBounds();
     }
 
     
@@ -20,10 +29,20 @@ public class PlayerController : MonoBehaviour
         MovePlayer();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
     void MovePlayer()
     {
         moveVector = moveAction.ReadValue<Vector2>();
+        Vector3 newPos = transform.position + moveVector * moveSpeed * Time.deltaTime;
 
-        transform.position += moveVector * moveSpeed * Time.deltaTime;
+        newPos.x = Math.Clamp(newPos.x, minBounds.x + leftBoundPadding, maxBounds.x - rightBoundPadding);
+        newPos.y = Math.Clamp(newPos.y, minBounds.y + downBoundPadding, maxBounds.y - upBoundPadding);
+        transform.position = newPos;
     }
 }

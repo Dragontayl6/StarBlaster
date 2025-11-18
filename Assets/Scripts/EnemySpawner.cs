f18eaece9f82af2e4eeb6c7d07b32ac2165e84f8
TaylorValk <Tvalk06@icloud.com>
2025-11-17 22:56:47 -0500

    While Loops

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,6 +5,7 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] WaveConfigSO[] waveConfigs;
     [SerializeField] float timeBetweenWaves = 1f;
+    [SerializeField] bool isLooping;
     WaveConfigSO currentWave;
 
     void Start()
@@ -14,21 +15,24 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnEnemies()
     {
-        foreach (WaveConfigSO wave in waveConfigs)
+        do
         {
-            currentWave = wave;
-            for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+            foreach (WaveConfigSO wave in waveConfigs)
             {
-                Instantiate(
-                    currentWave.GetEnemyPrefab(i), 
-                    currentWave.GetStartingWaypoint().position, 
-                    Quaternion.identity,
-                    transform);
+                currentWave = wave;
+                for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+                {
+                    Instantiate(
+                        currentWave.GetEnemyPrefab(i), 
+                        currentWave.GetStartingWaypoint().position, 
+                        Quaternion.identity,
+                        transform);
 
-                yield return new WaitForSeconds(currentWave.GetRandomEnemySpawnTime());
+                    yield return new WaitForSeconds(currentWave.GetRandomEnemySpawnTime());
+                }
+                yield return new WaitForSeconds(timeBetweenWaves);
             }
-            yield return new WaitForSeconds(timeBetweenWaves);
-        }
+        } while (isLooping);
     }
 
     public WaveConfigSO GetCurrentWave()

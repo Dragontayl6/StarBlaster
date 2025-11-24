dfc85711ac1a52612ad8baef16a46de87feb08ca
TaylorValk <Tvalk06@icloud.com>
2025-11-24 11:07:53 -0500

    Screen Shake

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,14 +3,30 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] int healthPoints = 50;
+    [SerializeField] ParticleSystem hitParticles;
+
+    [SerializeField] bool applyCameraShake;
+    CameraShake cameraShake;
+
+    void Start()
+    {   
+        cameraShake = Camera.main.GetComponent<CameraShake>();   
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
+        
         if (damageDealer != null)
         {
             TakeDamage(damageDealer.GetDamage());
+            PlayHitParticles();
             damageDealer.Hit();
+
+            if (applyCameraShake)
+            {
+                cameraShake.Play();
+            }
         }
     }
 
@@ -20,6 +36,16 @@ public class Health : MonoBehaviour
         if (healthPoints <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void PlayHitParticles()
+    {
+        if (hitParticles != null)
+        {
+            ParticleSystem particles = Instantiate(hitParticles, transform.position, Quaternion.identity);
+            particles.Play();
+            Destroy(particles, particles.main.duration + particles.main.startLifetime.constantMax);
         }
     }
 }
